3aa67e6b01ca8a4f649137d70cdea787b67b3048
Tonalicious <antonh13@ru.is>
2014-10-13 19:37:02 +0000

    fixed code for different delimiter test (7th test)

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,12 +3,16 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
+		String delimiters = ",|\n";
+
 		if (text.startsWith("//")) {
-			return 0;
+			delimiters = text.substring(2, 3);
+			text = text.substring(4);
+			return sum(splitNumbers(text, delimiters));
 		} else if(text.equals("")) {
 			return 0;
 		} else if(text.contains(",") || text.contains("\n")) {
-			return sum(splitNumbers(text)); 
+			return sum(splitNumbers(text, delimiters)); 
 		} else {
 			return 1;
 		}
@@ -18,8 +22,8 @@ public class Calculator {
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",|\n");
+	private static String[] splitNumbers(String numbers, String delimiters){
+	    return numbers.split(delimiters);
 	}
 
     private static int sum(String[] numbers){
